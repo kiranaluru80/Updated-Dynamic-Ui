3ff20c5462a7c6fbbac3e0e91679fd67c5fe4342
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2017-11-10 07:00:54 -0500

    updated dynaic ui

---
+++ SampleDB/SampleDB/CommunicationsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace SampleDB
+{
+    public partial class CommunicationsPage : ContentPage
+    {
+        public List<JSSEMasterBehavior> listOfObjects;
+        public CommunicationsPage()
+        {
+            InitializeComponent();
+            Debug.WriteLine(listOfObjects);
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listOfObjects = await App.Database.GetJsseBehaviors(0,1);
+
+
+        }
+    }
+}
